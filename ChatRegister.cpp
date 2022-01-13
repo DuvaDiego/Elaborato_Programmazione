93f8fed17e096aa538491e02e3563d6d95ca27ad
Diego D'Uva <diegoduva2000@gmail.com>
2022-01-13 16:40:00 +0100

    Aggiunta attributi principali alla classe ChatRegister

--- ChatRegister.cpp
+++ ChatRegister.cpp
@@ -1,1 +1,13 @@
 #include "ChatRegister.h"
+
+ChatRegister::ChatRegister(std::list<Chat *> list, std::string o) : chatList(move(list)), owner(move(o)) {
+}
+
+void ChatRegister::getChatList() {
+    for (auto& chat : chatList)
+        std::cout << chat->getName() << std::endl; //TODO aggiungere la caratteristica delle chat preferite che compaiono in cima al registro
+}
+
+std::string ChatRegister::getOwner() {
+    return owner;
+}
--- ChatRegister.h
+++ ChatRegister.h
@@ -1,11 +1,22 @@
 #ifndef ELABORATO_PROGRAMMAZIONE_CHATREGISTER_H
 #define ELABORATO_PROGRAMMAZIONE_CHATREGISTER_H
 
+#include <iostream>
+#include <list>
+#include "Chat.h"
 
 class ChatRegister {
 public:
-    ChatRegister() = default;
+    explicit ChatRegister(std::list<Chat*> list, std::string o = "Diego");
     ~ChatRegister() = default;
+
+    void getChatList();
+
+    std::string getOwner();
+
+private:
+    std::list<Chat*> chatList;
+    std::string owner;
 };
 
 
